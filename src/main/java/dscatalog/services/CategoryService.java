34fbc43f8e02a892d65bdf916aed3beeb9403664
Leonardo Capristo <leonardo.capristo@hotmail.com>
2025-03-18 15:58:02 -0300

    dto

--- src/main/java/dscatalog/services/CategoryService.java
+++ src/main/java/dscatalog/services/CategoryService.java
@@ -1,21 +1,25 @@
 package dscatalog.services;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import dscatalog.dto.CategoryDTO;
 import dscatalog.entities.Category;
 import dscatalog.repositories.CategoryRepository;
 
 @Service
 public class CategoryService {
 
-	
 	@Autowired
 	private CategoryRepository repository;
-	
-	public List<Category> findAll(){
-		return repository.findAll();
+
+	@Transactional(readOnly = true)
+	public List<CategoryDTO> findAll() {
+		List<Category> listCategory = repository.findAll();
+		return listCategory.stream().map(x -> new CategoryDTO(x)).collect(Collectors.toList());
 	}
 }
